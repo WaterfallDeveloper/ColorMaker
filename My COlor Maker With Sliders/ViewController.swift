4bd411842d0a504fd6de39cf4720025731cf405b
WaterfallDeveloper <joe.z@wolke7.net>
2015-11-01 09:49:42 +0100

    Initial Commit

---
+++ My COlor Maker With Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My COlor Maker With Sliders
+//
+//  Created by Joe Z on 01.11.15.
+//  Copyright © 2015 WaterfallDeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
